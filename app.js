d03e96cedc9bdde76bc6380127d0db21c7b72e5c
The Good Maxwell <maxwellraden@gmail.com>
2017-01-13 06:21:11 -0800

    starting

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// your code here!
+
+function averageSentence(text){
+	var sentences = text.split(".");
+	var total = 0;
+	for(i = 0; i < sentences.length; i++){
+		total += sentences[i].length;
+	}
+	var average = total / sentences.length;
+	$("#average-sentence").append(average);
+}
+
+function submissionListener() {
+  $('.js-text-form').submit(function(event) {
+    event.preventDefault();
+    var userText = $(this).find('#user-text').val();
+    reportOnText(removeReturns(userText));
+  });
+}
