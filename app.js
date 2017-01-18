d881ba9abb7637b6402ff69844fb79fa4748c2dc
The Good Maxwell <maxwellraden@gmail.com>
2017-01-18 15:24:05 -0800

    finished

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
+$(document).ready(function(){
 
-function averageSentence(text){
-	var sentences = text.split(".");
-	var total = 0;
-	for(i = 0; i < sentences.length; i++){
-		total += sentences[i].length;
+	function numberOfSentences(text) {
+		//var splitRegex = new RegExp(".!?");
+		var sentences = text.split(".");
+		return sentences;
 	}
-	var average = total / sentences.length;
-	$("#average-sentence").append(average);
-}
 
-function submissionListener() {
-  $('.js-text-form').submit(function(event) {
-    event.preventDefault();
-    var userText = $(this).find('#user-text').val();
-    reportOnText(removeReturns(userText));
-  });
-}
+	function averageSentence(text){
+		var sentences = numberOfSentences(text);
+		var total = 0;
+		for(i = 0; i < sentences.length; i++){
+			total += sentences[i].length;
+		}
+		var average = total / sentences.length;
+		$("#average-sentence").append(average);
+	}
+
+	function countWords(text){
+		var words = text.split(" ");
+		var wordCount = words.length;
+		var mySet = new Set(words);
+	    var uniqueWords = mySet.size();
+		
+		var wordTotal = 0;
+		for(i = 0; i < words.length; i++){
+			wordTotal += words[i].length;
+			}
+		var averageWord = wordTotal / wordCount;
+	    
+
+		
+	    $("#unique-words").append(uniqueWords);
+		$("#average-word").append(averageWord);
+		$("#word-count").append(wordCount);
+	}
+
+	$('#text-form').submit(function(event) {
+		event.preventDefault();
+		var text = $('#user-text').val();
+		averageSentence(text);
+		countWords(text);
+		$('.text-report').removeClass('hidden');
+
+	});
+
+});
